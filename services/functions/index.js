2d6366ce4de31d5de0b85aee30b0a7548dbc50a0
huynhnguyen <nguyenhuynh200188@gmail.com>
2018-12-22 20:59:06 +0700

    attempt to fix

--- services/functions/index.js
+++ services/functions/index.js
@@ -1,20 +1,13 @@
 const functions = require('firebase-functions');
 
 exports.records = functions.https.onRequest((request, response) => {
-});
-
-exports.insights = functions.https.onRequest((request, response) => {
-});
-
-exports.deploy = functions.https.onRequest((request, response) => {
-    const service = 
-    const params = 
-    const insight = await nlpIntegraton.SentimentInsight({text: 'hello, world'});
-    response.send(insight);
-});
-
-exports.subscribe = functions.https.onRequest((request, response) => {
 
 });
 
+exports.logs = functions.https.onRequest((request, response) => {
 
+});
+
+exports.messages = functions.https.onRequest((request, response) => {
+
+});
